a8458a3d9644e7ede4f47a4ecd9be5f6c71e11d6
Leisan <fleisan18@mail.ru>
2021-11-03 10:47:51 +0500

    HomeTask_Dog

---
+++ HomeTask_Dog/Program.cs
@@ -0,0 +1,28 @@
+﻿double t;
+double FFSpeed = 4;
+double SFSpeed = 3;
+double DogSpeed = 8;
+double S = 1000;
+int count = 0;
+int friend = 2; // 1 - первый друг, 2  - второй друг
+
+
+while(S >= 10)
+{
+    if(friend == 1)
+    {
+        t = S/(DogSpeed + FFSpeed);
+        friend = 2;
+    }
+    else
+    {
+        t=S/(DogSpeed + SFSpeed);
+        friend = 1;
+    }
+    S = S - t*(SFSpeed + FFSpeed);
+    Console.WriteLine($"Расстояние между друзьями стало: {S}");
+    count++;
+    Console.WriteLine($"На данном этапе собака пробежала: {count}");
+
+}
+Console.WriteLine($"Всего собака пробежала: {count}");
